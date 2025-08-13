486b1b11ef956c5dbb71ed2779c60ae7b254724c
Kassim Amir <amir.kassim946@gmail.com>
2025-08-13 10:03:13 +0300

    commit initial files of quizzical_app project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main-section'>
+      <h1 className='title'>Quizzical App</h1>
+    </main>
   );
 }
 
